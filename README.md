5aac0a489abb1fd8f48103d7cbf15457c75b3134
PEM-- <pemarchandet@gmail.com>
2014-11-12 23:40:17 +0100

    Fake require

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# BkImage
+# BkSurfaceImage - Adaptive images
 A plugin for [famous-views](http://famous-views.meteor.com).
 
 This plugin brings [famous-bksurfaceimage](https://github.com/IjzerenHein/famous-bkimagesurface) in Blaze or Jade templating for [Meteor.js](https://www.meteor.com).
 
-As the author of the component says it:
+As the author of the component, [Hein Rutjes](https://github.com/IjzerenHein) says it:
 > BkImageSurface adds support for sizing strategies such as AspectFit and AspectFill for displaying images with [famo.us](http://famo.us). It uses a `div` with a background-image rather than a `img` tag.
--- package.js
+++ package.js
@@ -1,17 +1,19 @@
 Package.describe({
-  name: 'pierreeric:fview-bkimage',
-  summary: ' /* Fill me in! */ ',
-  version: '1.0.0',
-  git: ' /* Fill me in! */ '
+  name: 'pierreeric:fview-bksurfaceimage',
+  summary: 'Adaptive image plugin for Meteor\'s famous-views.',
+  version: '0.1.0',
+  git: 'https://github.com/PEM--/fview-bksurfaceimage'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.addFiles('pierreeric:fview-bkimage.js');
-});
-
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('pierreeric:fview-bkimage');
-  api.addFiles('pierreeric:fview-bkimage-tests.js');
+  api.use('mjn:famous@0.3.0_5', 'client', { weak: true });
+  api.use('raix:famono@0.9.14', { weak: true });
+  api.use('gadicohen:famous-views@0.1.24');
+  api.addFiles(
+    [
+      'lib/pre.js',
+      'lib/famous-bkimagesurface/BkImageSurface.js',
+      'lib/post.js'
+    ], 'client');
 });
